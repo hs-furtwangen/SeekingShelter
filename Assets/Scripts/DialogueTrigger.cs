ce8772a8095db9b98bfe3989505113c6c0023250
Kubickeck <adrian.kubicek@web.de>
2019-01-26 12:37:46 +0100

    Typewriter Functionality added
    - Typewriter functionality added for Dialogue Box
    - Animator Variable added in DummyMovements

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText= "";
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowDialogue()); 
+    }
+
+    IEnumerator ShowDialogue()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0,i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
--- Assets/Scripts/DummyMovement.cs
+++ Assets/Scripts/DummyMovement.cs
@@ -6,15 +6,20 @@ public class DummyMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+
         if (GameController.Instance.CanMove)
         {
             rb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * 10);
@@ -23,5 +28,8 @@ public class DummyMovement : MonoBehaviour
         {
             rb.velocity = Vector2.zero;
         }
+
+
+        //anim.SetFloat("movement", moveHorizontal);
     }
 }
