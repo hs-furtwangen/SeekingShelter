dd4f2c56c1e6b05548defa00fe95f2329a1b5bf1
Kubickeck <adrian.kubicek@web.de>
2019-01-27 14:31:01 +0100

    Vorhang fixed

--- Assets/Scripts/VorhangScript.cs
+++ Assets/Scripts/VorhangScript.cs
@@ -23,15 +23,6 @@ public class VorhangScript : MonoBehaviour
     {
         if (Input.GetKeyDown("space")  && playerInside == true)
         {
-            if (isHiddenNow == true)
-            {
-                GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>().enabled = true;
-                GameController.Instance.GameStates["CanMove"] = true;
-                GameController.Instance.GameStates["isHidden"] = false;
-                closedVorhang.SetActive(false);
-                normalVorhang.SetActive(true);
-                isHiddenNow = false;
-            }
 
             if (isHiddenNow == false)
             {
@@ -41,6 +32,14 @@ public class VorhangScript : MonoBehaviour
                 closedVorhang.SetActive(true);
                 normalVorhang.SetActive(false);
                 isHiddenNow = true;
+            } else
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>().enabled = true;
+                GameController.Instance.GameStates["CanMove"] = true;
+                GameController.Instance.GameStates["isHidden"] = false;
+                closedVorhang.SetActive(false);
+                normalVorhang.SetActive(true);
+                isHiddenNow = false;
             }
         }
     }
