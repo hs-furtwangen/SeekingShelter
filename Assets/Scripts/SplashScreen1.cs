323ec5484632733a2af35bd23e85c8e3887bee96
Kubickeck <adrian.kubicek@web.de>
2019-01-27 12:20:47 +0100

    SplashScreen added
    - Splashscreen with rudimentary functions and Design uploaded

---
+++ Assets/Scripts/SplashScreen1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen1 : MonoBehaviour
+{
+
+    public GameObject SplashScreen;
+    public GameObject CreditsScreen;
+    public GameObject ControlScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SplashScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+        ControlScreen.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SplashScreen.SetActive(false);
+        CreditsScreen.SetActive(false);
+        ControlScreen.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        CreditsScreen.SetActive(true);
+        ControlScreen.SetActive(false);
+    }
+
+    public void GoBackToSplashScreen()
+    {
+        CreditsScreen.SetActive(false);
+        ControlScreen.SetActive(false);
+    }
+
+    public void OpenControls()
+    {
+        CreditsScreen.SetActive(false);
+        ControlScreen.SetActive(true);
+    }
+}
