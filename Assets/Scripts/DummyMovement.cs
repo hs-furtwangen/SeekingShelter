18a6a20f84051f2a1869751bbefd799fa2e22baf
Alexander Scheurer <mail@aspepex.net>
2019-01-27 19:15:54 +0100

    Fixes

--- Assets/Scripts/DummyMovement.cs
+++ Assets/Scripts/DummyMovement.cs
@@ -16,7 +16,7 @@ public class DummyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
